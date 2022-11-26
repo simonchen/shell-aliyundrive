a1c7f11e2aec5ffeb358d9d0f595d190c725221f
xychenxy <chenxinyi1978@hotmail.com>
2022-11-26 15:37:09 +0800

    Update aliyundrive_install.sh
    fixing variables / crontab replacement.

--- aliyundrive_install.sh
+++ aliyundrive_install.sh
@@ -43,13 +43,13 @@ fi
 
 download_url=https://github.com/messense/aliyundrive-webdav/releases/download/$latest_ver/aliyundrive-webdav-$latest_ver.$platform-unknown-linux-$arch_float_mode.tar.gz
 
-tmp_dir=/tmp/etc_storage_apps/
+tmp_dir=/tmp/etc_storage_apps
 
 logger -s -t "【 创建临时目录 】:" ""$tmp_dir""
 mkdir "$tmp_dir"
 
 logger -s -t "【 下 载 】" "$download_url"
-path=/tmp/etc_storage_apps/aliyundrive-webdav.tar.gz
+path=$tmp_dir/aliyundrive-webdav.tar.gz
 wget "$download_url" -O "$path"
 if [ "$?" != "0" ]; then
 	logger -s -t "【下载失败】" "1分钟后自动重试"
@@ -57,7 +57,8 @@ fi
 tar -xzf "$path" -C "$tmp_dir"
 rm "$path"
 
-cat >$tmp_dir/aliyundrive_watch.sh <<'EOF'
+watch_script=aliyundrive_watch.sh
+cat >$tmp_dir/$watch_script <<'EOF'
 #!/bin/sh
 # detecting if aliyun drive service is down
 
@@ -69,7 +70,7 @@ if [ "$?" == "0" ]; then
 else
         logger -s -t "【 阿里云盘异常, 重启 】" "aliyundrive-webdav."
 EOF
-cat <<EOF >> $tmp_dir/aliyundrive_watch.sh
+cat <<EOF >> $tmp_dir/$watch_script
 	killall "aliyundrive-webdav"
 	$basedir/$basename "$refresh_token"
 fi 
@@ -82,11 +83,12 @@ killall "aliyundrive-webdav"
 $tmp_dir/aliyundrive-webdav --host 0.0.0.0 -I --no-trash --no-redirect --no-self-upgrade -p 8080 -r $refresh_token -U admin -W admin > /dev/null &
 
 logger -s -t "【 监控aliyundrive 】" "start"
-if crontab -l | grep "aliyundrive_watch.sh"; then
-	logger -s -t "Cronjob exists: " "aliyundrive_watch.sh" 
+line="*/1 * * * * $tmp_dir/$watch_script"
+if crontab -l | grep "$watch_script"; then
+	logger -s -t "Replace Cronjob: " "$watch_script" 
+	(crontab -l | grep -v "$watch_script"; echo "$line" ) | crontab -
 else
-	logger -s -t "Running Cronjob: " "aliyundrive_watch.sh"
-	line="*/1 * * * * /tmp/etc_storage_apps/aliyundrive_watch.sh"
+	logger -s -t "Running Cronjob: " "$watch_script"
 	(crontab -l; echo "$line" ) | crontab -
 fi
 
