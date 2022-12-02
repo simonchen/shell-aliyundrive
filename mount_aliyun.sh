bb47185e3d4a25e065cb7d0a6a85e6cdab7ba9db
xychenxy <chenxinyi1978@hotmail.com>
2022-12-02 20:39:32 +0800

    Update mount_aliyun.sh

--- mount_aliyun.sh
+++ mount_aliyun.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+basedir=$(cd $(dirname $0) && pwd)
+
 modprobe fuse 
 mkdir /mnt/aliyun
 umount -fl /mnt/aliyun 
 killall "webdavfs"
 sleep 3
-/etc/storage/apps/webdavfs -D -ousername=admin,password=admin http://0.0.0.0:8080 /mnt/aliyun
+$basedir/webdavfs -D -ousername=admin,password=admin,ro,async_read http://0.0.0.0:8080 /mnt/aliyun
 
