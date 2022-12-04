bad80d7231f83fb182bc9a793710d08208d895ac
xychenxy <chenxinyi1978@hotmail.com>
2022-12-04 09:34:02 +0800

    Add files via upload

--- mount_aliyun.sh
+++ mount_aliyun.sh
@@ -6,5 +6,6 @@ mkdir /mnt/aliyun
 umount -fl /mnt/aliyun 
 killall "webdavfs"
 sleep 3
-$basedir/webdavfs -D -ousername=admin,password=admin,ro,async_read http://0.0.0.0:8080 /mnt/aliyun
+echo 35 > /proc/sys/vm/pagecache_ratio # by default, it's 50% ratio with page cache, it's too big!
+$basedir/webdavfs -D -ousername=admin,password=admin,ro,async_read,readbuff=1048576 http://0.0.0.0:8080 /mnt/aliyun
 
---
+++ start_samba.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+basedir=$(cd $(dirname $0) && pwd)
+
+logger -t "【启动SAMBA】" "samba"
+killall "smbd"
+rm /var/run/smbd-smb.conf.pid
+mkdir /etc/samba
+/bin/smbpasswd admin admin
+/sbin/smbd -D -s $basedir/smb.conf
+killall "nmbd"
+/sbin/nmbd -D -s $basedir/smb.conf
+
+logger -t "【SAMBA服务器】" "失效SAMBA访问"
+while ip_rule_num=$(iptables -L INPUT --line-numbers | grep netbios | cut -d" " -f1)
+do
+    if [ -z $ip_rule_num ]; then
+        break
+    fi
+    for n in $ip_rule_num; do
+        iptables -D INPUT $n
+        echo delete ip rule no.$n - ok
+        break
+    done
+done
+
+logger -t "【SAMBA服务器】" "允许SAMBA访问"
+iptables -I INPUT 1 -p udp -m multiport --dport 137,138 -j ACCEPT 
+iptables -I INPUT 1 -p tcp -m state --state NEW,RELATED,ESTABLISHED -m multiport --dport 139,445 -j ACCEPT
+iptables -I INPUT -p icmp -j ACCEPT
