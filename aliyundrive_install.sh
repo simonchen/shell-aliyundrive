4141d0ac463dcd745b718da0de30e0f0c2e862a9
xychenxy <chenxinyi1978@hotmail.com>
2022-12-21 14:15:14 +0800

    Update aliyundrive_install.sh

--- aliyundrive_install.sh
+++ aliyundrive_install.sh
@@ -80,7 +80,7 @@ chmod 777 $tmp_dir/*
 
 logger -s -t "【 启动aliyundrive 】" "start"
 killall "aliyundrive-webdav"
-$tmp_dir/aliyundrive-webdav --host 0.0.0.0 -I --no-trash --no-redirect --no-self-upgrade -p 8080 -r $refresh_token -U admin -W admin > /dev/null &
+$tmp_dir/aliyundrive-webdav --host 0.0.0.0 -I --no-trash --no-redirect --no-self-upgrade --read-buffer-size 1048576 --upload-buffer-size 1048576 -p 8080 -r $refresh_token -U admin -W admin > /dev/null &
 if [ -f $basedir/mount_aliyun.sh ]; then
         $basedir/mount_aliyun.sh
 fi
